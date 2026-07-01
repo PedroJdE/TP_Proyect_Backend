b1fd45bd2206f3383d00b65f45b2af06ca34efcb
pedro_jde <pedrojuliandeelias@gmail.com>
2026-07-01 20:50:52 -0300

    ultimoscambios0.0

--- backend/tests/mailerConfig.test.js
+++
@@ -1,9 +0,0 @@
-import test from 'node:test'
-import assert from 'node:assert/strict'
-
-import { normalizeGmailAppPassword } from '../src/config/mailer.config.js'
-
-test('quita los espacios de una contraseña de aplicación de Gmail', () => {
-  assert.equal(normalizeGmailAppPassword('ykba jbvd qwqi dgrf'), 'ykbajbvdqwqidgrf')
-  assert.equal(normalizeGmailAppPassword(' ykbajbvdqwqidgrf '), 'ykbajbvdqwqidgrf')
-})
